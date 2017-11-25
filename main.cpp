c01aa8a70a8dd9bd5e28e0a167417191b6cfda57
unknown <popya1337@gmail.com>
2017-11-25 15:34:34 -0500

    main function

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "----------------------" << endl;
+    cout << "Welcome to BlackJack!!" << endl;
+    cout << "----------------------" << endl;
+}
